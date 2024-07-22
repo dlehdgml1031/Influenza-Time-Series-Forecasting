574a9692a71c007db59658c22623a53488956104
dlehdgml1031 <dlehdgml1031@gmail.com>
2024-07-22 20:41:42 +0900

    add calculate_time_series_lag func

--- utils/time_series_analysis.py
+++ utils/time_series_analysis.py
@@ -1,12 +1,16 @@
+from timefeatures import date_to_year_week
+
 import os
 import sys
 import warnings
 
 import torch
 import numpy as np
-from scipy.signal import correlate
+import pandas as pd
+import statsmodels.api as sm
 
-def time_series_lag_analysis(
+# To be fixed
+def dymanic_time_series_lag_analysis(
     feature_data:torch.Tensor,
     target_data:torch.Tensor,
 ):
@@ -31,24 +35,59 @@ def time_series_lag_analysis(
     
     max_lag = pred_len
     
+    assert int(max_lag) >= 3, 'max_lag (pred_len) should be greater than or equal to 3'
+    
     feature_lags = torch.zeros(n_features)
     
-    # Loop over each feature
-    for feature_idx in range(n_features):
-        cross_corrs = np.zeros(max_lag)
+    # batch_size -> feature -> lag
+    # caluclate cross-correlation
+    
+    # Loop over each batch
+    for batch_idx in range(batch_size):
         
-        # Calculate cross-correlation for each lag
-        for lag in range(1, max_lag + 1):
-            corr_sum = 0
+        # Loop over each feature
+        for feature_idx in range(n_features):
+            cross_corrs = np.zeros(max_lag)
             
-            for batch_idx in range(batch_size):
+            # Loop over each lag
+            for lag in range(1, max_lag + 1):
+                corr_sum = 0
+                
                 feature_seq = feature_data[batch_idx, :, feature_idx].numpy()
                 target_seq = target_data[batch_idx, :, 0].numpy()
+                target_seq = target_seq[lag:]
                 
-                print(feature_seq)
-                print(target_seq)
-                return None
-            
+                feature_seq_mean = np.mean(feature_seq)
+                target_seq_mean = np.mean(target_seq)
+                
+                numerator = np.sum()
+                denominator = np.sum() * np.sum()
+                
+                corr = numerator / denominator
 
-if __name__ == '__main__':
-    pass
+
+def calculate_time_series_lag(
+    feature_data_path:str,
+    target_data_path:str,
+    feature_col_name:str,
+    target_col_name:str,
+    max_lags:int,
+):
+    if 'GT_influenza.csv' in feature_data_path:
+        feature_df = pd.read_csv(feature_data_path)
+    else:
+        feature_df = pd.read_csv(feature_data_path, index_col = 0)
+    target_df = pd.read_csv(target_data_path, index_col = [0,1])
+    
+    feature_df[['YEAR', 'WEEK']] = feature_df['Weekly_Date_Custom'].apply(lambda x: pd.Series(date_to_year_week(x)))
+    feature_df.set_index(['YEAR', 'WEEK'], inplace = True)
+    
+    target_df = target_df.loc[feature_df.index, :]
+    
+    x = feature_df.loc[:, feature_col_name].values
+    y = target_df.loc[:, target_col_name].values
+    
+    ccf = sm.tsa.stattools.ccf(x, y, nlags = max_lags, unbiased = False)
+    optimal_lag = np.argmax(ccf)
+    
+    return ccf, optimal_lag
